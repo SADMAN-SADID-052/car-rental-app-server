f47e9160c599eaec42e83414d174750c94de4024
SADMAN-SADID-052 <sadman.cse8.bu@gmail.com>
2025-01-22 00:25:37 +0600

    addCar to the server

--- index.js
+++ index.js
@@ -26,14 +26,30 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const carCollection = client.db("carrentalDB").collection("carRental");
+
+
+    // post add car
+    app.post("/carRental", async (req, res) => {
+      const newCar = req.body;
+      console.log(newCar);
+
+      const result = await carCollection.insertOne(newCar);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
